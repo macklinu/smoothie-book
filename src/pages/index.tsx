1617b65b881546ed75996a48f6c50722a1edb35d
Mackie Underdown <macklinu@users.noreply.github.com>
2021-04-29 14:15:43 -0400

    Implement logged out view

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,7 @@ import {
   BoxProps,
   Button,
   ButtonGroup,
+  Center,
   Container,
   Flex,
   FormControl,
@@ -16,6 +17,7 @@ import {
   IconButton,
   Image,
   Input,
+  Link,
   List,
   ListItem,
   Modal,
@@ -296,9 +298,35 @@ function CreateRecipeModal({ onClose, onSubmit }: CreateRecipeModalProps) {
   )
 }
 
-// TODO implement
 function LoggedOutView() {
-  return <div>Logged Out</div>
+  return (
+    <Container maxWidth='container.xl' py={4}>
+      <Center flexDirection='column'>
+        <Heading>Welcome to Smoothie Book</Heading>
+        <Text>Sign in to start collecting your smoothie recipes!</Text>
+        <Image
+          py={4}
+          src='https://images.unsplash.com/photo-1550461716-dbf266b2a8a7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=960&q=80'
+        />
+        <Text fontSize='small' color='gray.700'>
+          Photo by{' '}
+          <Link
+            color='blue.500'
+            href='https://unsplash.com/@gabriellefaithhenderson?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText'
+          >
+            Gabrielle Henderson
+          </Link>{' '}
+          on{' '}
+          <Link
+            color='blue.500'
+            href='https://unsplash.com/?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText'
+          >
+            Unsplash
+          </Link>
+        </Text>
+      </Center>
+    </Container>
+  )
 }
 
 export const getServerSideProps: GetServerSideProps = async ({ req }) => {
