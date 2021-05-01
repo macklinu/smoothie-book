d2e5383aff6dc29f2c4cd0345b3b57ba563c5bc1
Mackie Underdown <macklinu@users.noreply.github.com>
2021-05-01 10:38:24 -0400

    Add favicon and basic meta tags

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import { ChakraProvider } from '@chakra-ui/react'
 import { Provider } from 'next-auth/client'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 import { QueryClient, QueryClientProvider } from 'react-query'
 import { ReactQueryDevtools } from 'react-query/devtools'
 
@@ -12,6 +13,12 @@ function App({ Component, pageProps }: AppProps) {
       <QueryClientProvider client={queryClient}>
         <ChakraProvider>
           <>
+            <Head>
+              <title>Smoothie Book</title>
+              <meta charSet='utf-8' />
+              <meta name='viewport' content='initial-scale=1.0, width=device-width' />
+              <link rel='shortcut icon' href='/favicon.ico' />
+            </Head>
             <Component {...pageProps} />
             <ReactQueryDevtools initialIsOpen={false} />
           </>
